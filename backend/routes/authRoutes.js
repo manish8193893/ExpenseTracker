9c7f25c7e58207dc8e95c178abd13dd7147bf1e7
Manish Kumar <manish8193892@gmail.com>
2025-11-24 13:47:20 +0530

    update authRouter file

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,13 +13,13 @@ router.post('/login', loginUser);
 router.get('/getUser', protect, getUserInfo);
 
 // Route for getting user profile by ID
-router.post("/upload-image", upload.single("image"), async (req, res) => {
-   if(!req.file) {
-       return res.status(400).json({ message: 'No file uploaded' });
-   }
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    res.status(200).json({ imageUrl });
+});
 
-   const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
-   res.status(200).json({ imageUrl });
-})
 
 module.exports = router;
